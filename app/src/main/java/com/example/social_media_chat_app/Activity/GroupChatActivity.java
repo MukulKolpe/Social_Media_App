1cdaac0da4185ccd283edf01a671302738d31ff6
Mukul Kolpe <mukulkolpe45@gmail.com>
2021-11-22 21:03:40 +0530

    Minor changes

---
+++ app/src/main/java/com/example/social_media_chat_app/Activity/GroupChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.social_media_chat_app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.social_media_chat_app.R;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+    }
+}
